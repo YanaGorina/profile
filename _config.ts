a47d164e67773642e8b14e0c92c4076de9c8270d
Yana Gorina <yagorina.ne@gmail.com>
2023-11-05 20:05:52 -0800

    Initial project setup and configuration

---
+++ _config.ts
@@ -0,0 +1,18 @@
+import lume from "lume/mod.ts";
+import esbuild from "lume/plugins/esbuild.ts";
+// import favicon from "lume/plugins/favicon.ts";
+import jsx from "lume/plugins/jsx.ts";
+import jsx_preact from "lume/plugins/jsx_preact.ts";
+import tailwindcss from "lume/plugins/tailwindcss.ts";
+import postcss from "lume/plugins/postcss.ts";
+
+const site = lume({src: "./src"});
+
+site.use(esbuild());
+// site.use(favicon());
+site.use(jsx());
+site.use(jsx_preact());
+site.use(tailwindcss());
+site.use(postcss());
+
+export default site;
